2a575d29e6b77bcb72059fb99ada2e1400b438b3
dhil1512 <dhil.mail@protonmail.com>
2024-10-20 18:23:38 +0100

    Speeding up site

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Stars from "./Stars/Stars.jsx"
-import "./Stars/Stars.scss"
-
 import TopNav from "./TopNav/TopNav.jsx"
 import "./TopNav/TopNav.scss"
 import "./TopNav/themeChanger.scss"
