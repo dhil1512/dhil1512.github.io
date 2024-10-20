9efe6f4692e0c76894679a0e1bfb3986b342cd53
Dylan Wilson <61094712+dhil1512@users.noreply.github.com>
2024-10-20 20:03:49 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/dhil1512.github.io/', // Set the base path
+  base: "dylanw.co.uk", // Set the base path
 })
